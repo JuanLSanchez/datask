c1d45bf2e000374d91edea0106068ccfc1898cd3
Juan L. Sánchez <juanlu-28a@hotmail.com>
2017-06-13 20:29:28 +0200

    feat: add bugdetId and companyId fileds to the object in the list of project

--- src/main/java/es/juanlsanchez/datask/web/mapper/ProjectDTOMapper.java
+++ src/main/java/es/juanlsanchez/datask/web/mapper/ProjectDTOMapper.java
@@ -1,6 +1,8 @@
 package es.juanlsanchez.datask.web.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import es.juanlsanchez.datask.domain.Project;
 import es.juanlsanchez.datask.web.dto.ProjectDTO;
@@ -8,6 +10,8 @@ import es.juanlsanchez.datask.web.dto.ProjectDTO;
 @Mapper(componentModel = "spring")
 public interface ProjectDTOMapper {
 
+  @Mappings({@Mapping(target = "companyId", source = "company.id"),
+      @Mapping(target = "budgetId", source = "budget.id")})
   public ProjectDTO fromProject(Project project);
 
 }
